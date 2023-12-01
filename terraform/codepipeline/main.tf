3c939db0bcf264bff8915082ef2909aa668f3a9a
Kevin De Notariis <kevin.denotariis@gmail.com>
2023-12-01 11:06:03 +0000

    fix: Update s3 bucket configuration for new provider

--- terraform/codepipeline/main.tf
+++ terraform/codepipeline/main.tf
@@ -19,9 +19,20 @@ resource "aws_s3_bucket" "this" {
   tags = local.tags
 }
 
+resource "aws_s3_bucket_ownership_controls" "this" {
+  bucket = aws_s3_bucket.this.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
 resource "aws_s3_bucket_acl" "this" {
   bucket = aws_s3_bucket.this.id
   acl    = "private"
+
+  depends_on = [
+    aws_s3_bucket_ownership_controls.this
+  ]
 }
 
 resource "aws_s3_bucket_public_access_block" "this" {
